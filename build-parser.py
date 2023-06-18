54d874ec81a28c54c78a6ec896b5d164ee393d15
lnay <luke@grahamnaylor.net>
2023-06-18 17:46:22 +0100

    Add parser shared object build script

---
+++ build-parser.py
@@ -0,0 +1,9 @@
+from tree_sitter import Language
+
+Language.build_library(
+  # Store the library in the `build` directory
+  'build/sage.so',
+
+  # Include one or more languages
+  [ 'tree-sitter-sage/' ]
+)
